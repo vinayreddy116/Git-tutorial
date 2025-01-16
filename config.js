1fe76bc0a714f81b6f23cbcd2b00096ee60c414d
vinayreddy <vvinayreddy116@gmail.com>
2025-01-16 13:29:41 +0530

    "version2.0.0"

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('config.js loaded');
-console.log('version1');
+console.log('version2');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('index.js loaded');
-console.log('version1');
+console.log('version2');
