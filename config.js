3e5d55380a98195f29033cc7faee2b13a80f3246
vinayreddy <vvinayreddy116@gmail.com>
2025-01-16 13:32:14 +0530

    version3

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('config.js loaded');
-console.log('version2');
+console.log('version3');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('index.js loaded');
-console.log('version2');
+console.log('version3');
